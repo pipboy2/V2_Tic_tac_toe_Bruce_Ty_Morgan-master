3716e6c58ae72944f410c8e04cbad953d453b7cb
Tyler Workman <tylerworkintc@gmail.com>
2018-09-13 14:59:49 -0400

    Test

--- CodingActivity_TicTacToe_ConsoleGame.Solution/View/ConsoleView.cs
+++ CodingActivity_TicTacToe_ConsoleGame.Solution/View/ConsoleView.cs
@@ -10,7 +10,7 @@ namespace CodingActivity_TicTacToe_ConsoleGame
     public class ConsoleView
     {
         #region ENUMS
-
+        
         public enum ViewState
         {
             Active,
@@ -19,6 +19,8 @@ namespace CodingActivity_TicTacToe_ConsoleGame
         }
 
         #endregion
+
+        //a
 
         #region FIELDS
 
